e4d9e83f371b45c0e62461b4aeedf10466b3c508
shaqila <shaqila.erbeliza@outlook.com>
2023-08-22 10:37:23 +0700

    update intro

--- src/components/Fragments/SkillCard/index.jsx
+++ src/components/Fragments/SkillCard/index.jsx
@@ -62,9 +62,6 @@ export const SkillCard = () => {
                 {level}
               </div>
             </div>
-            <div className="group-hover:opacity-100 transition-opacity bg-gray-800 p-2 text-sm text-gray-100 rounded-md absolute left-1/2 -translate-x-1/2 opacity-0">
-              In your example the root parent div div1 has
-            </div>
           </motion.div>
         ))}
       </div>
