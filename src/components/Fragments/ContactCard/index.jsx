39b397ac9d495209efb62875a226f26c18c3dbbe
shaqila <shaqila.erbeliza@outlook.com>
2023-09-07 08:59:45 +0700

    update contact

--- src/components/Fragments/ContactCard/index.jsx
+++ src/components/Fragments/ContactCard/index.jsx
@@ -1,22 +1,22 @@
 /* eslint-disable react/prop-types */
 import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
 
 export const ContactCard = ({ icon, name, description, link }) => {
   return (
     <motion.div
-      className="flex overflow-hidden items-center space-x-4 w-full p-4 rounded-xl backdrop-filter backdrop-blur-sm bg-opacity-0 border border-gray-100 shadow-md"
+      className="flex overflow-hidden items-center space-x-4 w-full p-4 rounded-xl backdrop-filter backdrop-blur-sm bg-opacity-0 border border-gray-100 shadow-md cursor-pointer"
       whileHover={{ scale: 1.1 }}
+      onClick={() => window.open(link)}
+      target="_blank"
+      title={name + ": " + description}
     >
       <div>{icon}</div>
-      <Link to={link} target="_blank">
-        <div className="flex flex-col space-y-2 items-start text-slate-800 text-xs font-light md:font-semibold md:text-md">
-          <div>
-            <h2 className="font-bold ">{name}</h2>
-            <span>{description}</span>
-          </div>
+      <div className="flex flex-col space-y-2 items-start text-slate-800 text-xs font-light md:font-semibold md:text-md">
+        <div>
+          <h2 className="font-bold ">{name}</h2>
+          <span>{description}</span>
         </div>
-      </Link>
+      </div>
     </motion.div>
   );
 };
